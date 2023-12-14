2cb2694b3ae44b7de636de0afed4b4886dc11221
Sidharth Bhasin <sid21bhasin@gmail.com>
2023-12-14 14:34:24 +0530

    Components added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black w-full">
+      <span className="text-red-600 text-4xl w-40 font-bold m-5">CinemAi</span>
+    </div>
+  );
+};
+
+export default Header;
